c42cc85b2fec55ef41153b75d91e446b2ae7c336
Yang <229047054@qq.com>
2020-10-02 18:19:37 +0800

    即时聊天室

---
+++ Server.h
@@ -0,0 +1,44 @@
+#ifndef CHATROOM_SERVER_H
+#define CHATROOM_SERVER_H
+
+#include<string>
+#include"Common.h"
+
+using namespace std;
+
+//服务端类，用来处理客户请求
+class Server{
+
+    public:
+        //无参数构造函数
+        Server();
+
+        //初始化服务器段设置
+        void Init();
+
+        //关闭服务
+        void Close();
+
+        //启动服务端
+        void Start();
+    
+    private:
+        //广播消息给所有客户端
+        int SendBroadcastMessage(int clientfd);
+
+        //服务器端serverAddr信息
+        struct sockaddr_in serverAddr;
+
+        //创建监听的socket
+        int listener;
+
+        //epoll_create创建后的返回值
+        int epfd;
+
+        //客户端列表
+        list<int> client_list;
+
+};
+
+
+#endif
